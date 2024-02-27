bd4771af6000e825d5a5a7adbc18c80596d2f887
Stole Krshkov <krshkovstole@gmail.com>
2024-02-27 11:54:20 +0100

    Implementing simple style to the welcome page

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,16 +1,17 @@
 @extends('layouts.guest')
 @section('content')
 
-    <div class="_container">
-        <h2>Welcome to A must, simplistic, powerful, one and only</h2>
-        <h1>{{ config('app.name') }}</h1>
+    <div class="_container text-center">
+        <h3 class="mt-32">Welcome to <span class="color-main">A</span> must, simplistic, <span class="color-main">powerful</span>,
+            one and only</h3>
+        <h1 class="mt-8 color-main">{{ config('app.name') }}</h1>
 
-        <div>
+        <div class="mt-8">
             <a href="{{ route("login") }}">
-                <button>Login</button>
+                <button class="btn-main hoverable">Login</button>
             </a>
             <a href="{{ route("register") }}">
-                <button>Register</button>
+                <button class="btn-outline-main hoverable">Register</button>
             </a>
         </div>
     </div>
