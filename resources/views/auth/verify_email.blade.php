612b2e5de28bd4e3d6843cd0431be601a7430ee7
Stole Krshkov <krshkovstole@gmail.com>
2024-03-28 19:38:36 +0100

    small fix

--- resources/views/auth/verify_email.blade.php
+++ resources/views/auth/verify_email.blade.php
@@ -3,7 +3,7 @@
     <div class="_container flex justify-center items-center mt-12 color-main">
         <div class="max-w-form w-full">
             <h3 class="text-center">Verify Email</h3>
-            <p class="mt-8">In order to participate in events and get all the benefits for using BeautyRoom, you need to
+            <p class="mt-8">In order to participate in events and get all the benefits for using {{ config('app.name', 'Laravel Powerup') }}, you need to
                 confirm your email address.</p>
 
             <form action="{{ route('verification.send') }}" method="POST">
